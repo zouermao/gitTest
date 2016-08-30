772bdc56e35478892273cbca790d2c1ff98aa257
zouermao <2590581563@qq.com>
2016-08-30 17:10:13 +0800

    跟新

---
+++ app/src/main/java/com/example/zoujun/git/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.zoujun.git;
+
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    int a=0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+}
